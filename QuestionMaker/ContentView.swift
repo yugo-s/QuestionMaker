edc57cfd0ba0b573dad4f24196626477d92a2548
yugo-s <y.saka0423@icloud.com>
2024-11-24 11:44:26 +0900

    Initial Commit

---
+++ QuestionMaker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QuestionMaker
+//
+//  Created by 坂口 友吾 on 2024/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QuestionMaker/QuestionMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuestionMakerApp.swift
+//  QuestionMaker
+//
+//  Created by 坂口 友吾 on 2024/11/24.
+//
+
+import SwiftUI
+
+@main
+struct QuestionMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
