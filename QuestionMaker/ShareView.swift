987a8824b20f661c1933be9ab796322b739ae341
yugo-s <y.saka0423@icloud.com>
2025-01-12 12:00:10 +0900

    tabview基本

---
+++ QuestionMaker/ShareView.swift
@@ -0,0 +1,18 @@
+//
+//  ShareView.swift
+//  QuestionMaker
+//
+//  Created by 坂口 友吾 on 2024/12/16.
+//
+
+import SwiftUI
+
+struct ShareView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShareView()
+}
---
+++ QuestionMaker/TestView.swift
@@ -0,0 +1,18 @@
+//
+//  TestView.swift
+//  QuestionMaker
+//
+//  Created by 坂口 友吾 on 2025/01/12.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestView()
+}
